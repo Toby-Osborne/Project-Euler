672a5e2baa23c99e517d85c5e1365e2bd35872c5
Toby-Osborne <tosb789@aucklanduni.ac.nz>
2021-09-29 19:19:36 +1300

    Q12 completed

--- Q1/Q12.cpp
+++ Q1/Q12.cpp
@@ -10,9 +10,27 @@ int main(){
     clock_t begin = clock();
     double time_spent = 0.0;
     
-    //what is the first triangle number to have over 500 divisors?
+    int count = 0;
+    int triNumber = 0;
+    int divisors = 0;
 
+    //what is the first triangle number to have over 500 divisors?
     
+    while (divisors < 500) {
+        count++;
+        divisors = 2;
+        triNumber+=count;
+        //cout << (int)sqrt((double)triNumber) << endl;
+        for (int i = 2;i<=(int)sqrt((double)triNumber);i++) {
+            if ((triNumber % i) == 0) {
+                divisors += 2;
+            }
+        }
+        //cout << triNumber << endl;
+        
+    }
+    
+    cout << triNumber << endl;
 
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
