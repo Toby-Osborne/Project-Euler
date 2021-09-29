a8e07edd6183c91ed8d855564da72308089bbe53
Zachary Hankin <44932159+koanarec@users.noreply.github.com>
2021-09-29 21:01:34 +1300

    Update Q9.cpp
    Divides the runtime by 17. The big O goes from n^3 to n ^2, while increasing the processing time in each loop. Instead of looping through what C could be, I am defining C as from a and b. It is clearly is worth it when you compare the runtimes. Still worth investigating the order of the if statement on line 19.

--- Q1/Q9.cpp
+++ Q1/Q9.cpp
@@ -12,23 +12,21 @@ int main(){
     //Clock Begin
     clock_t begin = clock();
     double time_spent = 0.0;
-    
-    for (int c = 1;c<1000;c++){
-        for (int b = 1;b<c;b++) {
-            for (int a=1;a<b;a++) {
-                if ((a+b+c==1000)&&(pow((double)b,2)+pow((double)a,2)==pow((double)c,2))) {
-                    cout << (a*b*c) << endl;
-                    clock_t end = clock();
-                    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-                    cout << "Time Spent: " << time_spent << endl;
-                    return 0; 
-                }
-            }
+    double c;
+    for (int a = 1;a<1000;a++){
+        for (int b = 1;b<1000;b++) {
+            c = sqrt(a * a + b * b);
+            if (a+b+c==1000 && a < b && b < c) {
+                cout << (a*b*c) << endl;
+                clock_t end = clock();
+                time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+                cout << "Time Spent: " << time_spent << endl;
+                return 0; }
         }
     }
 
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    cout << "Time Spent: " << time_spent << endl;
+    cout << "Time Spent: bad " << time_spent << endl;
     return 0; 
 }
